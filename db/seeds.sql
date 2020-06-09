37e6497140d4429332485b55123b48fff1ab28f0
Ryan Carr <ryanfcarr@gmail.com>
2020-06-09 17:58:13 -0400

    Database and SQL files completed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Default Burger One');
+INSERT INTO burgers (burger_name) VALUES ('Default Burger Two');
+INSERT INTO burgers (burger_name) VALUES ('Default Burger Three');
