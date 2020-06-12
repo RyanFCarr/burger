c8ec702fd1c1f23072d1d02280344241be315955
Ryan Carr <ryanfcarr@gmail.com>
2020-06-11 23:52:01 -0400

    completed

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const db;
+if (process.env.JAWSDB_URL) {
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+ db = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "simplepw",
     database: "burgers_db"
   });
+}
+
   
   db.connect((err) => {
     if (err) throw err;
